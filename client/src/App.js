74430547f8f39fddeec3b4996b0f8de006dda75f
Christopher Bruder <cbruder1292@gmail.com>
2020-05-28 14:11:32 -0500

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/Login';
+import Navbar from './pages/Home';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Login></Login>
     </div>
   );
