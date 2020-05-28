31159e8831f7646514467ff7469d2c444c5a8845
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-28 13:38:57 -0500

    Added html routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import {BrowserRouter as PageRouter, Route} from "react-router-dom";
+
 import Login from './pages/Login';
-// import './App.css';
+import Home from './pages/Home';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <PageRouter>
+      <div className="App">
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/home" component={Home}/>
+      </div>  
+    </PageRouter>
   );
 }
 
