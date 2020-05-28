21b592d0323dd78952cd6ff7bb1bbd6bd39a66f1
mauraparker02 <mauraparker02@gmail.com>
2020-05-28 14:08:00 -0400

    got login page to render

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './pages/Login';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
