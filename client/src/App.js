f80a0a6a6d15522fc73c3f1d415336aa4d679eb8
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-06-01 13:06:37 -0500

    Login feature now functional.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import {BrowserRouter as PageRouter, Route} from "react-router-dom";
-import Navbar from "./components/Navbar/index.js";
-import Jumbotron from "./components/Jumbotron"
+import React, { Component } from 'react';
+import axios from "axios";
 
-
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
 
 import './App.css';
 
-function App() {
-  return (
-   <div>
-        <Navbar />
-        <Jumbotron />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      username: null,
+      user: null
+    }
 
-  </div>  
-    
-  );
+    this.getUser = this.getUser.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  updateUser(userObject) {
+    this.setState(userObject);
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ');
+      console.log(response.data);
+      if (response.data.user) {
+        console.log(response.data.user);
+        console.log('Get User: There is a user saved in the server session: ');
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username,
+          user: response.data.user
+        });
+      }
+      else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn}/>
+        <Jumbotron />
+        {this.state.loggedIn && <p>Hello, {this.state.username}!</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/Components/Jumbotron/index.js
+++ client/src/Components/Jumbotron/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Modal, Button } from 'react-materialize';
 
 function Jumbotron() {
   return (
