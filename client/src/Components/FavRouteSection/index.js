cf646f87834c7b0286c1074b4baec74e7e39779d
mauraparker02 <mauraparker02@gmail.com>
2020-06-08 21:48:08 -0400

    fixing the padding issue

--- client/src/Components/FavRouteSection/index.js
+++ client/src/Components/FavRouteSection/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Row, Col, } from 'react-materialize';
 import FavRouteCard from "./FavRouteCard";
+import './style.css' 
 
 
 
@@ -9,7 +10,7 @@ function FavRouteSection(props) {
         <div>
             {props.routes.map(route =>
                 <Row>
-                    <div class="bg-color-test">
+                    <div>
                         <Col m={3} />
                         <Col m={6}><FavRouteCard username={props.username} route={route} /></Col>
                         <Col m={3} />
